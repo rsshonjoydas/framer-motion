d90fbed5681959f68aa3f092e493796301c18bd8
rsshonjoydas <rsshonjoydas@gmail.com>
2022-01-14 00:39:06 +0600

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+const Home = () => (
+  <div>
+    <h1 className="h-screen flex items-center justify-center bg-gray-100 text-lg font-semibold text-purple-500 font-cursive">
+      RS Shonjoy
+    </h1>
+  </div>
+);
+
+export default Home;
